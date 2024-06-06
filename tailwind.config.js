edec52ab8ec7a1e9b68cad270208815e2074bee5
mateuszAF <mateusz.pytlak@af.agency>
2024-06-06 11:55:02 +0200

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from "tailwindcss/defaultTheme"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
+}
+
