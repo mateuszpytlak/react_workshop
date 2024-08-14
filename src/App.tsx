62af01a435c48a904c1bc54d8a014e503c03f9eb
mateuszAF <mateusz.pytlak@af.agency>
2024-08-14 14:38:12 +0200

    lesson 1.16 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { Button } from './ui/Button/Button';
+import { Text } from './ui/Text/Text';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Text>Test text</Text>
+      <Button label="Hello world!" onClick={ () => alert('haj') } disabled={true} />
     </>
   )
 }
---
+++ src/ui/Button/Button.tsx
@@ -0,0 +1,22 @@
+import { type ComponentProps } from "react";
+import { cn } from "../../utils/cn"
+
+type Props = {
+    label: string;
+    // onClick: MouseEventHandler<HTMLButtonElement>;
+} & ComponentProps<'button'>;
+
+export function Button ( { label, className, ...rest }: Props ) {
+    return (
+        <button 
+            className={cn(
+                "px-4 py-1 text-sm text-white bg-blue-600 rounded-sm border cursor-pointer border-blue-200 hover:bg-blue-500 disabled:bg-slate-400 disabled:cursor-not-allowed", 
+                className,
+                // {"bg-red-500": disabled}
+            )}
+            {...rest}
+        >
+            { label }
+        </button>
+    )
+}
