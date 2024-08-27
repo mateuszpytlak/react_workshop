b1912fe0b3eef5cbb372fc5d533d9a327541e536
mateuszAF <mateusz.pytlak@af.agency>
2024-08-27 20:55:47 +0200

    form based on refs

---
+++ src/components/RegistrationForm/RegistrationFormRefs.tsx
@@ -0,0 +1,45 @@
+import {Button} from "../../ui/Button/Button.tsx";
+import { FormEventHandler, useRef } from "react";
+
+type RegistrationFormData = {
+    email: string;
+    password: string;
+    language: string;
+}
+
+export const RegistrationFormRefs = () => {
+
+    const emailFieldRef = useRef<HTMLInputElement>(null);
+    const passwordFieldRef = useRef<HTMLInputElement>(null);
+    const languageFieldRef = useRef<HTMLInputElement>(null);
+
+
+    const handleSubmit: FormEventHandler = (event) => {
+        event.preventDefault();
+        const data: RegistrationFormData = {
+            email: emailFieldRef.current?.value || '',
+            password: passwordFieldRef.current?.value || '',
+            language: languageFieldRef.current?.value || '',
+        }
+        console.log(data); //send to API
+    }
+
+
+    return (
+        <form action="" onSubmit={handleSubmit}>
+            <div className="my-2">
+                <label htmlFor="email" className="mr-2">E-mail</label>
+                <input id="email" ref={emailFieldRef} type="email"/>
+            </div>
+            <div className="my-2">
+                <label htmlFor="password" className="mr-2">Password</label>
+                <input id="password" ref={passwordFieldRef} type="password"/>
+            </div>
+            <div className="my-2">
+                <label htmlFor="language" className="mr-2">Language</label>
+                <input id="language" ref={languageFieldRef}/>
+            </div>
+            <Button label="Send" type="submit"></Button>
+        </form>
+    )
+}
