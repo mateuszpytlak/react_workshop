41681689e236b45597dff3febc0f98fae3e444c0
mateuszAF <mateusz.pytlak@af.agency>
2025-01-02 19:55:47 +0100

    task1 almost done (component generator)

---
+++ src/components/ComponentGenerator/index.ts
@@ -0,0 +1,1 @@
+export { ComponentGenerator } from "./ComponentGenerator";
---
+++ src/ui/Select/Select.tsx
@@ -0,0 +1,20 @@
+import {ComponentPropsWithRef, forwardRef, Ref} from "react";
+
+type Props = {
+    name: string
+} & ComponentPropsWithRef<'select'>;
+export const Select = forwardRef( ({name, ...rest}: Props,ref: Ref<HTMLSelectElement>) => {
+
+    return (
+        <div className="my-4">
+            <label htmlFor="html-select">Choose an element: </label>
+
+            <select ref={ref} {...rest} name={name} id="html-select">
+                <option value="">--Please choose an option--</option>
+                <option value="button">Button</option>
+                <option value="text">Text</option>
+                <option value="header">Header</option>
+            </select>
+        </div>
+    )
+});
