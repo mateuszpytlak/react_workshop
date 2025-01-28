ee5460a54a35e0292834cefeb82528429e3b529d
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-01-28 17:48:45 +0100

    custom hook ViewPort

---
+++ src/hooks/useViewPort.ts
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react";
+
+const getSize = () => {
+    return (
+        {
+            x: window.innerWidth,
+            y: window.innerHeight
+        }
+    )
+}
+export const useViewPort = () => {
+    const [size, setSize] = useState(getSize());
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize(getSize());
+        }
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    return size;
+}
