66cffb57bf2ea2232870b312f477c54aa1f2f137
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-02-01 17:00:07 +0100

    themeSwitcher on useRef not state

--- src/components/Theme/ThemeContext.tsx
+++ src/components/Theme/ThemeContext.tsx
@@ -1,12 +1,12 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useRef } from "react";
 
-enum Theme {
+export enum Theme {
     LIGHT = 'light',
     DARK = 'dark'
 }
 
 type ThemeContextType = {
-    theme: Theme,
+    theme: React.MutableRefObject<Theme>,
     toggle: () => void,
 }
 
@@ -17,28 +17,40 @@ export const useThemeContext = () => {
     const context = useContext(ThemeContext);
     if (context === null) {
         throw new Error('Component should by in ThemeContextProvider!')
-    };
+    }
     return context;
 };
 
-const useTheme = () => {
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
 
-    const toggle = () => {        
-        if (theme === Theme.LIGHT) {
-            setTheme(Theme.DARK)
-            document.body.classList.add('dark');           
+const useTheme = () => {
+    //const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+    const theme = useRef<Theme>(Theme.LIGHT);
+
+    // const toggle = () => {        
+    //     if (theme === Theme.LIGHT) {
+    //         setTheme(Theme.DARK)
+    //         document.body.classList.add('dark');           
+    //     } else {
+    //         setTheme(Theme.LIGHT)
+    //         document.body.classList.remove('dark');
+    //     }
+    // }
+
+    const toggle = () => {
+        if (theme.current === Theme.LIGHT) {
+            document.body.classList.add('dark');
+            theme.current = Theme.DARK
         } else {
-            setTheme(Theme.LIGHT)
             document.body.classList.remove('dark');
+            theme.current = Theme.LIGHT
         }
     }
 
     return { theme, toggle };
 }
 
-export const ThemeContextProvider = ( {children} : {children: React.ReactNode}) => {
-    
+export const ThemeContextProvider = ({ children }: { children: React.ReactNode }) => {
+
     return (
         <ThemeContext.Provider value={useTheme()}>
             {children}
