949b3bfc70b14fe5a337fe54fba315ecae3ae607
mateuszAF <mateusz.pytlak@af.agency>
2025-02-01 15:34:52 +0100

    themeContextSwitcher

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -6,13 +6,13 @@ type Props = {
     // onClick: MouseEventHandler<HTMLButtonElement>;
 } & ComponentProps<'button'>;
 
-export function Button ( { label, className, ...rest }: Props ) {
+export function Button ( { label, className, disabled, ...rest }: Props ) {
     return (
         <button 
             className={cn(
-                "px-4 py-1 text-sm text-white bg-blue-600 rounded-sm border cursor-pointer border-blue-200 hover:bg-blue-500 disabled:bg-slate-400 disabled:cursor-not-allowed", 
+                "px-4 py-1 text-sm text-white bg-blue-600 dark:bg-green-600 rounded-sm border cursor-pointer border-blue-200 hover:bg-blue-500 disabled:bg-slate-400 disabled:cursor-not-allowed", 
                 className,
-                // {"bg-red-500": disabled}
+                {"bg-red-500": disabled}
             )}
             {...rest}
         >
