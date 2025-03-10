19c9391438cf47b4e8a8859463643243072c1c24
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-03-10 12:39:11 +0100

    custom hook z sortowaniem

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,3 +1,4 @@
+import { Direction, useSortData } from "../../hooks/sortData";
 import { Text } from "../../ui";
 import { type User } from "../../utils/mockData";
 
@@ -5,49 +6,8 @@ type Props = {
     data: User[];
 }
 
-type DataKey = keyof User;
-enum Direction {
-    ASC = 'ascending',
-    DESC = 'descending',
-}
-
 export const UsersList = ({ data }: Props) => {
-
-    const sortData = (data: User[], field: DataKey, direction?: Direction) => {
-        return data.sort((a, b) => {
-            const nameA = a[field];
-            const nameB = b[field];
-
-            if (direction === Direction.ASC) {
-                if (nameA < nameB) {
-                    return -1;
-                }
-                if (nameA > nameB) {
-                    return 1;
-                }
-            } else {
-                if (nameA < nameB) {
-                    return 1;
-                }
-                if (nameA > nameB) {
-                    return -1;
-                }
-    
-            }
-
-            if (nameA < nameB) {
-                return -1;
-            }
-            if (nameA > nameB) {
-                return 1;
-            }
-
-            // names must be equal
-            return 0;
-        });
-    }
-
-    const sortedData = sortData(data, 'nickname', Direction.DESC);
+    const sortedData = useSortData(data, 'age', Direction.DESC);
 
     return (
         <div>
