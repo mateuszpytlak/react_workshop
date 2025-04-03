619b3deffb6659d7db2d023021d1b89c2fdd36eb
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-04-03 19:21:35 +0200

    added isLoading and isError api flags

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -5,16 +5,34 @@ import { Text } from "../ui"
 import { fetchProducts } from "../services/products";
 
 export const ProductsPage = () => {
-    const [products, setProducts] = useState<ProductDto[]>([])
+    const [data, setData] = useState<ProductDto[] | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
+    //alternative:
     useEffect(() => {
-        fetchProducts().then(data => setProducts(data.records))
+        const loadData = async () => {
+            try {
+                const responseData = await fetchProducts();
+                setData(responseData.records);
+                setIsLoading(false);
+            } catch {
+                setIsError(true);
+            }
+        };
+        loadData();
     }, []);
+
+    // useEffect(() => {
+    //     fetchProducts().then(responseData => setData(responseData.records))
+    // }, []);
 
     return (
         <>
             <Text>Products List</Text>
-            <ProductsList products={products} />
+            {isLoading && <p className="text-white">Loading...</p>}
+            {isError && <p className="text-red-600">Oh no! Error!</p>}
+            {data && <ProductsList products={data} />}
         </>
     )
 
