993b2eb869e35de89136fab50959f356c001a67b
mateuszAF <mateusz.pytlak@af.agency>
2025-04-15 20:35:52 +0200

    add product api

--- src/pages/CreateProductPage.tsx
+++ src/pages/CreateProductPage.tsx
@@ -1,10 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { CreateProductForm } from "../features/Products/CreateProductForm"
+import { CreateProduct } from "../services/products";
+import { CreateProductDto } from "../types/Product"
+import { routes } from "../routes";
 
 export const CreateProductPage = () => {
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (data: CreateProductDto) => {
+        await CreateProduct(data);
+        navigate(routes.PRODUCTS.path);
+    }
+
     return (
         <>
             <h1 className="dark:text-slate-300">Create product</h1>
-            <CreateProductForm />
+            <CreateProductForm onSubmit={handleSubmit} />
         </>
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,3 @@
 export const HomePage = () => {
-    return <h1 className="dark:text-slate-300">Hello from HomePage! {import.meta.env.VITE_AIRTABLE_API_TOKEN}</h1>
+    return <h1 className="dark:text-slate-300">Hello from HomePage!</h1>
 }
