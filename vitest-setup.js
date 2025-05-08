cd38b6388600541cd6d92c63274449093412ff7c
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-05-08 21:04:45 +0200

    add test config

--- vitest-setup.js
+++ vitest-setup.js
@@ -3,8 +3,10 @@ import {cleanup} from "@testing-library/react";
 // import * as matchers from "@testing-library/jest-dom/matchers";
 import "@testing-library/jest-dom/vitest";
 import "@testing-library/jest-dom";
+import {toHaveNoViolations} from "jest-axe";
 
-expect.extend(matchers);
+// expect.extend(matchers);
+expect.extend(toHaveNoViolations);
 
 afterEach( () => {
     cleanup();
