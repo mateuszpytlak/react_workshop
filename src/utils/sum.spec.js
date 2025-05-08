457de09b97269362c41fbc5a464db19f4c7f2da4
mateuszAF <mateusz.pytlak@af.agency>
2025-05-08 21:00:42 +0200

    add @testing-library/jest-dom, @testing-library/react, jsdom and initial config

---
+++ src/utils/sum.spec.js
@@ -0,0 +1,17 @@
+import { expect, test, describe, it } from "vitest";
+import {sum} from "./sum.js";
+
+describe('Sum function', () => {
+    test("adds 1 + 2 to equal 3", () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    test("adds 1 + 'abc' to equal 1abc", () => {
+        expect(sum(1, 'abc')).toBe('1abc');
+    });
+
+    it("should equals 3", () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+})
+
