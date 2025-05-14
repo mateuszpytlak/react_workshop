e94652daa9cdf7d1db58e642763707a1d49e3b2e
mateuszAF <mateusz.pytlak@af.agency>
2025-05-14 18:14:53 +0200

    add <Button /> test

--- src/ui/Button/Button.spec.tsx
+++ src/ui/Button/Button.spec.tsx
@@ -1,1 +1,22 @@
-describe('')
+import {render, screen} from "@testing-library/react";
+import { axe } from 'jest-axe';
+import {Button} from "./Button.tsx";
+
+describe('Button component', () => {
+    it('should render correctly', () => {
+        //const { debug } = render(<Button label="Click me" />);
+        render(<Button label="Click me" />);
+
+        const elem = screen.getByText('Click me');
+        // debug();
+        expect(elem).toBeInTheDocument();
+    })
+
+    it('should have no violations', async () => {
+        const { container } = render(<Button label="Click me" />);
+
+        const result = await axe(container);
+
+        expect(result).toHaveNoViolations();
+    })
+})
