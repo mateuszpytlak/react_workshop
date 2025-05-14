667a8ef8ac39dccf257d6febf2fd86fc2883ca4f
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-05-14 18:14:43 +0200

    add input test

---
+++ src/ui/Input/Input.spec.tsx
@@ -0,0 +1,11 @@
+import {Input} from "./Input.tsx";
+import {render} from "@testing-library/react";
+import {axe} from "jest-axe";
+
+describe('Input component', () => {
+    it('should not have any accessibility violations', async () => {
+        const { container } = render(<Input label="E-mail" />);
+        const result = await axe(container);
+        expect(result).toHaveNoViolations();
+    })
+});
